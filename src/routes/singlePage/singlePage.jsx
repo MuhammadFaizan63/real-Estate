9aecf5f60e8e0ef3a603f250f8f340748b6ea875
smartfaizan63-pixel <smartfaizan63@gmail.com>
2026-05-14 20:52:57 +0500

    kam akhir ma ha

--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -30,20 +30,89 @@ export default function SinglePage() {
                     </div>
                 </div>
             </div>
-            <div className="features"></div>
-            <div className="wrapper">
-                {/* <p className="title">General</p>
-                <div className="listVertical"></div>
-                <p className="title">Sizes</p>
-                <div className="sizes"></div>
-                <p className="title">Nearby Places</p>
-                <div className="listHorizontal"></div>
-                <p className="title">Location</p>
-                <div className="mapContainer">
-                    <Map items={[singlePostData]} />
-                </div> */}
+            <div className="features">
+
+                <div className="wrapper">
+                    <p className="title">General</p>
+                    <div className="listVertical">
+                        <div className="feature">
+                            <img src="/utility.png" alt="" />
+                            <div className="featureText">
+                                <span>Utilities</span>
+                                <p>Renter is responsible</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="/pet.png" alt="" />
+                            <div className="featureText">
+                                <span>Pet Policy</span>
+                                <p>Pets Allowed</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="/fee.png" alt="" />
+                            <div className="featureText">
+                                <span>Property Fees</span>
+                                <p>Must have 3x the rent in total hosuehold income</p>
+                            </div>
+                        </div>
+                    </div>
+                    <p className="title">Sizes</p>
+                    <div className="sizes">
+                        <div className="size">
+                        <img src="/size.png" alt="" />
+                        <span>80sqft</span>
+                        </div>
+                    
+                        <div className="size">
+                        <img src="/bed.png" alt="" />
+                        <span>2 beds</span>
+                        </div>
+                    
+                    
+                        <div className="size">
+                        <img src="/bath.png" alt="" />
+                        <span>1 bathroom</span>
+                        </div>
+                    </div>
+                    
+                    <p className="title">Nearby Places</p>
+                    <div className="listHorizontal">
+                        <div className="feature">
+                            <img src="/school.png" alt="" />
+                            <span>School</span>
+                            <p>250m away</p>
+                        </div>
+                        <div className="feature">
+                            <img src="/pet.png" alt="" />
+                            <div className="spanOne">
+                            <span>Resturants</span>
+                            <p>250m away</p>
+                            </div>
+                        </div>
+                        <div className="feature">
+                            <img src="/pet.png" alt="" />
+                            <span>School</span>
+                            <p>250m away</p>
+                        </div>
+                    </div>
+                    <p className="title">Location</p>
+                    <div className="mapContainer">
+                        <Map items={[singlePostData]} />
+                    </div>
+                    <div className="buttons">
+                        <button>
+                            <img src="/chat.png" alt="" />
+                            Send a Message
+                        </button>
+                        <button>
+                            <img src="/chat.png" alt="" />
+                            Save the Place
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-        
+
     )
 }
